60d7a146484fd123004eac4ff387ce08883b1612
blackshane <122633142+blackshane@users.noreply.github.com>
2023-04-24 16:11:44 -0500

    fixed save feature. delete button not quite functional.

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,15 +1,18 @@
+
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/notes.html') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
   newNoteBtn = document.querySelector('.new-note');
   noteList = document.querySelectorAll('.list-container .list-group');
+
 }
 
 // Show an element
@@ -67,15 +70,27 @@ const renderActiveNote = () => {
 };
 
 const handleNoteSave = () => {
+  console.log('handleNoteSave working')
   const newNote = {
     title: noteTitle.value,
     text: noteText.value,
   };
-  saveNote(newNote).then(() => {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
+  saveNote(newNote)
+    .then(() => {
+      noteTitle.value = '';
+      noteText.value = '';
+      getNotes()
+        .then((res) => renderNoteList(res))
+        .catch((error) => {
+          console.error('Error occurred while retrieving notes:', error);
+        });
+      renderActiveNote();
+    })
+    .catch((error) => {
+      console.error('Error occurred while saving note:', error);
+    });
 };
+
 
 // Delete the clicked note
 const handleNoteDelete = (e) => {
@@ -118,6 +133,7 @@ const handleRenderSaveBtn = () => {
 
 // Render the list of note titles
 const renderNoteList = async (notes) => {
+  console.log('renderNoteList called with notes:', notes);
   let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
@@ -165,15 +181,24 @@ const renderNoteList = async (notes) => {
     noteListItems.push(li);
   });
 
-  if (window.location.pathname === '/notes') {
+  if (window.location.pathname === '/notes.html') {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
 };
 
 // Gets notes from the db and renders them to the sidebar
-const getAndRenderNotes = () => getNotes().then(renderNoteList);
+const getAndRenderNotes = () => {
+  getNotes()
+    .then((notes) => {
+      console.log('getNotes returned with notes:', notes);
+    renderNoteList(notes);
+    })
+    .catch((error) => {
+      console.error('Error occurred while getting and rendering notes:', error);
+    });
+};
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/notes.html') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
@@ -181,5 +206,6 @@ if (window.location.pathname === '/notes') {
 }
 
 getAndRenderNotes()
+
 
 
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const PORT = 3001;
-const data = require('../db/db.json');
 const path = require('path');
+let data = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8'));
+
 const { v4: uuidv4 } = require('uuid');
 
 // Sets up the Express app to handle data parsing
@@ -11,28 +12,44 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // For serving up static files in the 'public' folder. 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // GET route for api/notes endpoint.
-app.get('/api/notes', (req, res) => res.json(data));
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+  console.log('GET request for notes received.');
+});
 
 // POST route for api/notes endpoint.
 app.post('/api/notes', (req, res) => {
-  const newNote = {
-    id: uuidv4(),
-    title: req.body.title,
-    text: req.body.text
-  };
-  data.push(newNote);
-  fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(data), err => {
-    if (err) throw err;
-    res.json(newNote);
-  });
+  if (req.body) {
+    const newNote = {
+      id: uuidv4(),
+      title: req.body.title,
+      text: req.body.text
+    };
+
+    data.push(newNote);
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(data), err => {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      res.json(newNote);
+      console.log('POST request for notes received.', data);
+    });
+  } else {
+    console.log('POST request received with no body.');
+    res.status(400).send('Bad Request');
+  }
 });
+
+
   
 app.listen(PORT, () =>
 console.log(`Example app listening at http://localhost:${PORT}`)
 );
+
 
 
 // cd into DEVELOP before npm start so it can find package-json file.  
